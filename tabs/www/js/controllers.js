d39ec7d8b543da519eadb982cb9cda80ce35c363
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-24 00:27:10 -0300

    Jugabilidad terminada
    Ya quedó andando el programa.
    Falta:
    - Cambiar nombres templates
    - Login
    - Icon y SplashScreen
    - Enviar resultados a Firebase
    - Recuperar ranking resultados

--- tabs/www/js/controllers.js
+++ tabs/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $timeout) {
+.controller('DashCtrl', function($scope, $timeout, $cordovaVibration) {
   
     //Creo un objeto referencia a la BD en Firebase, a la 'tabla' de Preguntas
     var preguntasRef = new Firebase('https://triviapp-c9b77.firebaseio.com/preguntas');
@@ -9,64 +9,123 @@ angular.module('starter.controllers', [])
     $scope.infofirebase = [];
     $scope.respuestasCorrectas = {};
     $scope.pregunta = {};
+    $scope.preguntasHechas = [];
+    
+    //Banderas de estado
+    $scope.mostrarBotonEmpezar = true;
+    $scope.sePuedeEmpezar = false;
+    $scope.acierto = false;
+
+    //Contadores de puntuación
+    var contadorAciertos = 0;
+    var contadorPreguntas = 0;
     
     preguntasRef.on('child_added', function(snapshot){
         $timeout(function(){
             var pregunta = snapshot.val();
             $scope.infofirebase.push(pregunta);
             $scope.respuestasCorrectas[pregunta.id] = pregunta.respuesta;
+            $scope.sePuedeEmpezar = true;
         });
     });    
 
-    //Mostrar por consola lo que está almacenado en infofirebase
-    $scope.Empezar = function(){
-      //No sé cómo hacer para esperar a que se carguen todas las preguntas del Firebase y así sacar el length.
-      var aleatoria = Math.floor((Math.random() * 4));
-      //Muestro que tomé el JSON de una pregunta
-      console.info($scope.infofirebase[aleatoria]);
-      //Se lo asigno a la variable $scope.pregunta
-      $scope.pregunta = $scope.infofirebase[aleatoria];
-      //Muestro que lo asigné correctamente, y que ahora está cargada con una pregunta aleatoria
-      console.info($scope.pregunta);
+    //Tomo al azar una pregunta y la hago
+    $scope.HacerPregunta = function(){
+      $scope.mostrarBotonEmpezar = false;
+      $scope.acierto = false;
+      $scope.respuestaIncorrecta = false;
+
+      if($scope.preguntasHechas.length < 4){
+        //No sé cómo hacer para esperar a que se carguen todas las preguntas del Firebase y así sacar el length.
+        var aleatoria;
+        do{
+          aleatoria = Math.floor((Math.random() * 4)) //Entre 0 y 3
+          console.info("Id: ",aleatoria);
+        }while($scope.Evaluar(aleatoria, $scope.preguntasHechas));
+        //Se lo asigno a la variable $scope.pregunta
+        $scope.pregunta = $scope.infofirebase[aleatoria];
+        //Muestro que tomé el JSON de una pregunta, que lo asigné correctamente, y que ahora está cargada con una pregunta aleatoria
+        console.info($scope.pregunta);
+        //Guardo el id de la pregunta que salió, para que no se repita
+        $scope.preguntasHechas.push(aleatoria);
+        console.info($scope.preguntasHechas.length);
+      }
+      else{
+        console.log("Juego terminado");
+        $scope.Finalizar();
+      }
     }
 
+    $scope.Evaluar = function(valor, array){
+      var existe = false;
+      array.forEach(function(value){
+        if(value===valor){
+          existe = true;
+        }
+      })
+      return existe;
+    };
     
     //Creo un objeto de respuestas elegidas, donde dinámicamente voy a ir almacenando las respuestas seleccionadas.
     $scope.respuestasElegidas = {};
     
     
     //Programo el botón Enviar evaluando las respuestas elegidas con las correctas
-    $scope.Enviar = function(){
-        //alert("Sus respuestas fueron: " + JSON.stringify($scope.respuestasElegidas));
-        var contadorAciertos = 0;
-        var contadorPreguntas = 0;
+    $scope.Enviar = function(opcion){
         
-        for(var respuesta in $scope.respuestasCorrectas){
-            
-            contadorPreguntas ++;
-            
-            if ($scope.respuestasCorrectas[respuesta] === $scope.respuestasElegidas[respuesta]){
-                alert("Respuesta "+respuesta+": BIEN");
-                contadorAciertos ++;
-            }
-            else{
-                alert("Respuesta "+respuesta+": MAL");   
-            }
+        //Muestro la opción elegida
+        console.info(opcion);
+        //Pinto de verde la correcta (independientemente si acertó)
+        $scope.acierto = $scope.pregunta.respuesta;
+        //Incremento en una las preguntas jugadas
+        contadorPreguntas ++;
+        
+        //Evalúo si fue correcta
+        if($scope.pregunta.respuesta === opcion){
+            //CORRECTA
+            console.log("Correcta");
+            //Hago vibrar una vez
+            try{
+                $cordovaVibration.vibrate(200);
+            }catch (err){ console.log("No es un dispositivo", err); }
+            //Sumo una correcta
+            contadorAciertos ++;
         }
-        
-        alert("Cantidad de aciertos: "+contadorAciertos+"/"+contadorPreguntas);
-    }
+        else{
+            //INCORRECTA
+            console.log("Incorrecta");
+            //Vibrar
+            try{
+                $cordovaVibration.vibrate([150, 100, 100]);
+            }catch (err){ console.log("No es un dispositivo", err); }
+            //La pinto de rojo
+            $scope.respuestaIncorrecta = opcion;
+        }
+
+        //Lanzo una nueva pregunta que no se repita
+        $timeout(function(){
+            $scope.HacerPregunta();
+        },1500);
+    };
+
+    $scope.Finalizar = function(){
+        //Muestro resultados
+        alert("Juego terminado\nCantidad de aciertos: "+contadorAciertos+"/"+contadorPreguntas);
+        //Guardar los resultados y subirlos al Firebase
+        var resultados = {};
+        resultados.nombre = "Lautaro"
+        resultados.puntaje = contadorAciertos;
+        resultados.porcentaje = contadorAciertos/contadorPreguntas*100;
+        alert(JSON.stringify(resultados));
+        //Pongo en cero las variables necesarias para volver a empezar
+        contadorAciertos = 0;
+        contadorPreguntas = 0;
+        $scope.preguntasHechas = [];
+        $scope.mostrarBotonEmpezar = true;
+    };
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
